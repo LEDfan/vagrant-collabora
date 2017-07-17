9dcf0f657c5b44758b500cecff45432d50f1adea
Tobia De Koninck <tobia@inuits.eu>
2017-07-17 16:24:24 +0200

    Add option to manage ca

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,5 +2,8 @@ class { 'collabora':
   manage_repos    => true,
   storage_backend => 'wopi',
   wopi_host       => 'nextcloud.local',
-  servername      => 'collabora.local'
+  servername      => 'collabora.local',
+  manage_ca       => true,
+  #ca_key_file     => "puppet:///modules/collabora/ca.key.pem",
+  #ca_cert_file    => "puppet:///modules/collabora/ca.cert.pem"
 }
